16d3379ec18f2a6a8270620ff0777540c13ec058
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-06-25 22:01:55 +0300

    Updated index.js demo.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 import AbsoluteCoordinate from "./util/coordinate/AbsoluteCoordinate.mjs";
 import GridCoordinate from "./util/coordinate/GridCoordinate.mjs";
 import Grid from "./util/game/Grid.mjs";
-import BlockColorScheme from "./util/color/BlockColorScheme.mjs";
-import TetrisBlock from "./entity/TetrisBlock.mjs";
+import ITetrimino from "./entity/tetrimino/ITetrimino.mjs";
 
 const canvas = document.getElementById("gameScreen");
 const ctx = canvas.getContext("2d");
@@ -10,13 +9,32 @@ const ctx = canvas.getContext("2d");
 const GAME_WIDTH = canvas.width;
 const GAME_HEIGHT = canvas.height;
 
-const grid = new Grid(GAME_WIDTH, GAME_HEIGHT, 350, new AbsoluteCoordinate(0, 0));
+const grid = new Grid(GAME_WIDTH, GAME_HEIGHT, 35, new AbsoluteCoordinate(0, 0));
 
-const colorScheme1 = new BlockColorScheme("#0000aa", "#b3b3e6", "#000077", "#000033", "#4d4dc4");
-const colorScheme2 = new BlockColorScheme("#ff7700", "#ffd6b3", "#b25300", "#4c2300", "#ffa04d");
-const tetrisBlockTest1 = new TetrisBlock(new GridCoordinate(0, 0), colorScheme1);
-const tetrisBlockTest2 = new TetrisBlock(new GridCoordinate(0, 1), colorScheme2);
+const iTetrimino = new ITetrimino(new AbsoluteCoordinate(3, 0));
 
-tetrisBlockTest1.draw(ctx, grid);
-tetrisBlockTest2.draw(ctx, grid);
+iTetrimino.draw(ctx, grid);
 
+iTetrimino.moveDown();
+iTetrimino.moveDown();
+iTetrimino.draw(ctx, grid);
+
+iTetrimino.moveDown();
+iTetrimino.moveDown();
+iTetrimino.moveLeft();
+iTetrimino.moveLeft();
+iTetrimino.draw(ctx, grid);
+
+iTetrimino.moveDown();
+iTetrimino.moveDown();
+iTetrimino.moveRight();
+iTetrimino.moveRight();
+iTetrimino.moveRight();
+iTetrimino.moveRight();
+iTetrimino.draw(ctx, grid);
+
+iTetrimino.moveDown();
+iTetrimino.moveDown();
+iTetrimino.moveDown();
+iTetrimino.moveDown();
+iTetrimino.draw(ctx, grid);
