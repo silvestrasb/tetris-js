4506fce26ac061ada74ce5c5b093f562da5de7e1
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-06-27 05:49:27 +0300

    Added rotation feature to TetrisBlock class.

--- js/entity/TetrisBlock.mjs
+++ js/entity/TetrisBlock.mjs
@@ -1,6 +1,6 @@
 import Block from "./Block.mjs";
 
-export default class TetrisBlock extends Block{
+export default class TetrisBlock extends Block {
   /** Variable "#aspectRatio" should be private static, but js doesn't have a feature
    * where object's methods could access static variables,
    * only static methods (functions) static variables.
@@ -10,7 +10,36 @@ export default class TetrisBlock extends Block{
 
   constructor(coordinate, colorScheme) {
     super(coordinate, colorScheme);
-    }
+  }
+
+  /**
+   *
+   * @param axis axis on which the block will be rotated
+   * @param rotaionAngle degree of rotation
+   * @param boolCoordinateSystem true if coordinate, false or empty if block.
+   * {
+   * If its block system - then the block will be rotated on a "block" system,
+   * if its coordinate system - the the block will be rotated on a "coordinate" system.
+   * Default is false.
+   * If this sounds confusing, pm me.
+   * }
+   */
+  rotate(axis, boolCoordinateSystem = false, rotaionAngle = Math.PI / 2) {
+    const xDiff = this.coordinate.x - axis.x;
+    const yDiff = this.coordinate.y - axis.y;
+    const rotationPreference = boolCoordinateSystem ? -1 : 0;
+    this.coordinate.x =
+      Math.round(
+        Math.cos(rotaionAngle) * xDiff - Math.sin(rotaionAngle) * yDiff
+      ) +
+      axis.x +
+      rotationPreference;
+
+    this.coordinate.y =
+      Math.round(
+        Math.sin(rotaionAngle) * xDiff + Math.cos(rotaionAngle) * yDiff
+      ) + axis.y;
+  }
 
   /** Draws the border (4 triangles) of the tetris block. */
   _drawBorders(ctx, grid) {
