118006d55731091e145fcda5785fc50690247d21
sbumblauskas <silvestras.bumblauskas@gmail.com>
2023-05-11 15:39:08 +0300

    Node.js server start-up file using Express.js.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Tetris application listening on port ${port}`)
+});
+
+console.log('Server started at http://localhost:' + port);
